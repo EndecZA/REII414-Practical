a2991ac35711e8349875a3aa654ff073ac0b9621
Emile van Vuuren <112513707+EndecZA@users.noreply.github.com>
2026-04-26 19:46:01 +0200

    Added home layout design

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Class</title>
-</head>
 <body>
-Hello world (its me again)
-</script>
+<?php include 'navbar.php'; ?>
 
+    <h2>Phases Tracking Risk</h2>
+
+    <table border="1" width="100%">
+        <tr>
+            <th>Task</th>
+            <th>Person</th>
+            <th>Status</th>
+            <th>Deadline</th>
+            <th>Notes</th>
+        </tr>
+        <tr>
+           
+        </tr>
+    </table>
 </body>
 </html>
---
+++ navbar.php
@@ -0,0 +1,14 @@
+<table width="100%" border="0">
+    <tr>
+        <td valign="middle"> <img src="logo.jpeg" alt="Ahoy Logo" width="50" align="middle">&nbsp; |&nbsp; <a href="index.php">Home</a>&nbsp;
+            |&nbsp; 
+            <a href="projects.php">Projects</a>&nbsp;
+            |&nbsp;
+            <a href="notifications.php">Notifications</a>
+        </td>
+        
+        <td align="right" valign="middle"> Good evening: User
+        </td>
+    </tr>
+</table>
+<hr>
